b998f0fb51fce472391d53207bb2da453e54bb05
Shritesh Jamulkar <shritesh.sj@gmail.com>
2021-11-10 14:21:44 +0530

    first commit

---
+++ src/test/java/com/crossmesh/meshersolidity/Mesher/MesherSolidityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crossmesh.meshersolidity.Mesher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesherSolidityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
